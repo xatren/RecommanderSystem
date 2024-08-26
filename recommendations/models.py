d837bd9bb5e6b32f4237d87ffe4bcc0e6fa6537a
xatren <emircanyuksel24@gmail.com>
2024-08-26 17:58:49 +0300

    Add User, Item, and Rating models and setup database

--- recommendations/models.py
+++ recommendations/models.py
@@ -1,3 +1,17 @@
+# recommendations/models.py
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+    class Meta:
+        unique_together = ('user', 'item')
